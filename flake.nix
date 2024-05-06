e05e052bb4fb7ebaebfd9b8127fe78ff750155d2
Shubadus <crshumer@gmail.com>
2024-05-06 15:43:22 -0400

    Flake Troubleshooting

--- flake.nix
+++ flake.nix
@@ -22,32 +22,43 @@
     ...
   } @ inputs: let
     inherit (self) outputs;
-
       system = "x86_64-linux";
       pkgs = inputs.nixpkgs.legacyPackages.${system};
+      lib = nixpkgs.lib;
       # pkgs = import nixpkgs {
       #   inherit system;
       #   config = {
       #     allowUnfree = true;
       #   };
       # };
-      lib = nixpkgs.lib;
+
+      nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+
+      nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+
+      home-manager = {
+        url = "github:nix-community/home-manager/release-23.11";
+        inputs.nixpkgs.follows = "nixpkgs";
+      };
+
+      hyprland.url = "github:hyprwm/Hyprland";
   in {
     nixosConfigurations = {
-      it-vd-03 = nixpkgs.lib.nixosSystem {
-        specialArgs = { inherit system };
-        modules = [ default.nix ./hosts/it-lt-03 ];
+      it-vd-03 = lib.nixosSystem {
+        modules = [ ./hosts/it-lt-03/default.nix ];
       };
-      shumer-desktop = {
-        specialArgs = { inherit system };
-        modules = [ default.nix ./hosts/shumer-desktop ];
-      }
+     nixos = lib.nixosSystem {
+        modules = [ ./hosts/it-lt-03/default.nix ];
+      };
+      shumer-desktop = lib.nixosSystem {
+        modules = [ ./hosts/shumer-desktop/default.nix ];
+      };
     };
-  };
-  homeConfigurations.cshumer = home-manager.lib.homeManagerConfiguration {
-    inherit pkgs;
+    homeConfigurations.cshumer = home-manager.lib.homeManagerConfiguration {
+      inherit pkgs;
   
-    modules = [ ./modules/home/default.nix ];
+      modules = [ ./modules/home/default.nix ];
+    };
   };
 }
 
